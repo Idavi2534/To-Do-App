7574342a231f645eecad4310f45404b5ab58b1b2
Idavi2534 <idavi015@fiu.edu>
2019-03-28 11:41:29 -0400

    need to create login and connect to db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  todoArray=[]
+
+  addTodo(value){
+    this.todoArray.push(value)
+  }
+  
+  deleteItem(todo){
+    for(let i=0 ;i<= this.todoArray.length ;i++){
+     if(todo== this.todoArray[i]){
+      this.todoArray.splice(i,1)
+     }
+    }
+   }
 }
